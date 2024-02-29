4ffb14e9659913c2303519a78b68469c255f04f4
Noam VOLTAIRE <noam.voltaire@gmail.com>
2024-02-29 18:03:52 +0100

    cleaning split.c

--- utils/split.c
+++ utils/split.c
@@ -6,7 +6,7 @@
 /*   By: noam <noam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 16:44:08 by noam              #+#    #+#             */
-/*   Updated: 2024/02/29 17:52:45 by noam             ###   ########.fr       */
+/*   Updated: 2024/02/29 18:00:55 by noam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <unistd.h>
 
 
-static char *skiptonext(char *s, char sep)
+static char *advance_to_next_word(char *s, char sep)
 {
     while (*s && *s == sep)
         s++;
@@ -27,13 +27,13 @@ static	int	wordcount(char *s, char sep){
     int count;
 
     count = 0;
-    s = skiptonext(s, sep);
+    s = advance_to_next_word(s, sep);
     while (*s)
     {
         count++;
         while (*s && *s != sep)
             s++;
-        s = skiptonext(s, sep);
+        s = advance_to_next_word(s, sep);
     }
     return (count);
 }
@@ -47,7 +47,7 @@ static	char	*word_to_string(char *s, char sep)
     int word_len;
     
     word_len = 0;
-    word = skiptonext(s, sep);
+    word = advance_to_next_word(s, sep);
     s = word;
     while (*s && *s != sep)
     {
@@ -76,7 +76,7 @@ char	**split(char *str_of_words, char sep)
 	arr_of_words = (char **)malloc(sizeof(char *) * (wordcount(str_of_words, sep)));
 	if (arr_of_words == NULL)
 		return (NULL);
-    s = skiptonext(str_of_words, sep);
+    s = advance_to_next_word(str_of_words, sep);
     i=0;
 	while (*s)
 	{
@@ -84,40 +84,11 @@ char	**split(char *str_of_words, char sep)
         while (*s && *s != sep)
             s++;
 		i++;
-		s=skiptonext(s, sep);
+		s=advance_to_next_word(s, sep);
     }
 	return (arr_of_words);
 }
-/*
-char	**ft_split(char const *s, char c)
-{
-	int		j;
-	int		i;
-	char	**tab;
 
-	i = 0;
-	j = 0;
-	if (s == NULL)
-		return (NULL);
-	tab = (char **)malloc(sizeof(char *) * (wordcount((char *)s, c) + 2));
-	if (tab == NULL)
-		return (NULL);
-	while (s[i])
-	{
-		while (s[i] && (s[i] == c))
-			i++;
-		if (s[i] && (s[i] != c))
-		{
-			tab[j] = word_to_string((char *)s, c, i);
-			j++;
-		}
-		while (s[i] && (s[i] != c))
-			i++;
-	}
-	tab[j] = 0;
-	return (tab);
-}
-*/
 /* ************************************************************************** */
 
 int	main(void)
@@ -134,7 +105,6 @@ int	main(void)
         printf("word %d: %s\n", i, arr[i]);
         i++;
     }
-   
     
     
     // printf("wordcount %d\n", wordcount(" ", ' '));
